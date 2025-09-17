9969f13237016569c72dd7fca5bf42674c8525b9
timi2506 <172171055+timi2506@users.noreply.github.com>
2025-09-17 10:03:48 +0200

    Initial Commit

---
+++ SchoolTool/SchoolToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolToolApp.swift
+//  SchoolTool
+//
+//  Created by Tim Schuchardt on 17.09.25.
+//
+
+import SwiftUI
+
+@main
+struct SchoolToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
